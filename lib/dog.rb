d92712e53825b086deebb787da492fa9abfe5868
Javi Leyva <jvrleyva@gmail.com>
2020-06-03 21:09:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
   def initialize(name, breed = "Mut")	 
-    @name = name	    @name = name
-    @breed = breed	    @breed = breed
-  end	  end
+    @name = name	    
+    @breed = breed	    
+  end	  
+  
